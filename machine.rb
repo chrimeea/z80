5b9c9cc5ebf75cf21fe05ba8606f3034b7953c87
Cristian Mocanu <chrimeea@yahoo.com>
2022-10-31 22:59:13 +0200

    08

--- machine.rb
+++ machine.rb
@@ -12,7 +12,7 @@ class Z80
         @FLAG_Z = 0x40
         @FLAG_S = 0x80
         @a = @b = @c = @d = @e = @f = @h = @l = 0
-        @a2 = @b2 = @c2 = @d2 = @e2 = @f2 = @h2 = @l2 = 0
+        @a’ = @b’ = @c’ = @d’ = @e’ = @f’ = @h’ = @l’ = 0
         @sp = 0
         @pc = 0
         @i = 0
@@ -28,24 +28,24 @@ class Z80
 
     def run
         t = Time.now
+        t_states = 4
         op_size = 1
         case @memory[@pc]
-        when 0x00
-            t_states = 4
-        when 0x01
+        when 0x00 #NOP
+        when 0x01 #LD BC,HHLL
             @b, @c = @memory[@pc + 2], @memory[@pc + 1]
             t_states = 10
             op_size = 3
-        when 0x02
+        when 0x02 #LD (BC),A
             @memory[pair @b, @c] = @a
             t_states = 7
-        when 0x03
+        when 0x03 #INC BC
             @c += 1
             if @c == @MAX7
                 @b, @c = (@b + 1) & (@MAX7 - 1), -1
             end
             t_states = 6
-        when 0x04
+        when 0x04 #INC B
             @b += 1
             @f ^= @FLAG_N
             if @b == @MAX7
@@ -64,8 +64,7 @@ class Z80
                     end
                 end
             end
-            t_states = 4
-        when 0x05
+        when 0x05 #DEC B
             @b -= 1
             @f |= @FLAG_N
             if @b - 1 == -@MAX7
@@ -86,11 +85,20 @@ class Z80
                     end
                 end
             end
-            t_states = 4
-        when 0x06
+        when 0x06 #LD B,NN
             @b = @memory[@pc + 1]
             t_states = 7
             op_size = 2
+        when 0x07 #RLCA
+            @a *= 2
+            @f ^= @FLAG_N | @FLAG_HC
+            if @a.negative?
+                @f |= @FLAG_C
+            else
+                @f ^= @FLAG_C
+            end
+        when 0x08 #EX AF,AF’
+            @a, @f, @a’, @f’ = @a’, @f’, @a, @f
         else
             fail
         end
