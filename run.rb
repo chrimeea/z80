68883ea9fc931e33cbe50974813028c31f134ad1
Cristian Mocanu <chrimeea@yahoo.com>
2023-01-11 22:25:10 +0200

    tests

--- run.rb
+++ run.rb
@@ -3,14 +3,14 @@
 require 'test/unit'
 require_relative 'machine'
 
-# Z80::Hardware.new.boot
-z80 = Z80::Z80.new
-z80.memory.load_rom('./roms/hc90.rom')
-40.times do
-    reg = z80.fetch_opcode
-    puts reg
-    z80.execute reg
-    puts z80
-end
+Z80::Hardware.new.boot
+# z80 = Z80::Z80.new
+# z80.memory.load_rom('./roms/hc90.rom')
+# 40.times do
+#     reg = z80.fetch_opcode
+#     puts reg
+#     z80.execute reg
+#     puts z80
+# end
 
 #TODO: UART
