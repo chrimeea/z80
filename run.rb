0d1e0b3ef929a434ebe90dfb94a60fea6bd98038
Cristian Mocanu <chrimeea@yahoo.com>
2023-01-30 23:32:49 +0200

    tests

--- run.rb
+++ run.rb
@@ -12,10 +12,10 @@ z80.memory.load_rom('./roms/hc90.rom')
     z80.execute z80.fetch_opcode
     z80.execute z80.fetch_opcode until z80.pc.byte_value == 0x0E5E
 end
-# z80.execute z80.fetch_opcode until z80.pc.byte_value == 0x0E5E
+z80.execute z80.fetch_opcode until z80.pc.byte_value == 0x0EA0
 # puts Time.now - t
 # z80.pc.store_byte_value(0x38)
-30.times do
+100.times do
     puts z80.pc
     reg = z80.fetch_opcode
     puts reg
