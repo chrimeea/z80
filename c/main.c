5f7d4c6f3aad7ceb2a37174f1907dcacd740be9e
Cristian Mocanu <chrimeea@yahoo.com>
2024-03-25 18:07:04 +0200

    c

--- c/main.c
+++ c/main.c
@@ -1519,7 +1519,7 @@ int z80_execute_cb(REG8 reg)
         register_set_or_unset_bit(*alt, 0x01 << (reg.byte_value >> 3 & 0x07), true);
         return hl ? 15 : 8;
     default:
-        return z80_execute_simple(reg);
+        return 0; // fail
     }
 }
 
@@ -1785,9 +1785,8 @@ int z80_execute_ed(REG8 reg)
     case 0xDD:
     case 0xED:
     case 0xFD:
-        return 0;
     default:
-        return z80_execute_simple(reg);
+        return 0; // fail
     }
 }
 
@@ -2078,6 +2077,65 @@ int z80_execute_dd_fd(REG8 reg, REG16 *other)
             register_set_or_unset_flag(FLAG_HC, true);
             register_set_or_unset_flag(FLAG_N, false);
             return 20;
+        case 0x80: // LD r,RES b,(IX+d)
+        case 0x81:
+        case 0x82:
+        case 0x83:
+        case 0x84:
+        case 0x85:
+        case 0x87:
+        case 0x88:
+        case 0x89:
+        case 0x8A:
+        case 0x8B:
+        case 0x8C:
+        case 0x8D:
+        case 0x8F:
+        case 0x90:
+        case 0x91:
+        case 0x92:
+        case 0x93:
+        case 0x94:
+        case 0x95:
+        case 0x97:
+        case 0x98:
+        case 0x99:
+        case 0x9A:
+        case 0x9B:
+        case 0x9C:
+        case 0x9D:
+        case 0x9F:
+        case 0xA0:
+        case 0xA1:
+        case 0xA2:
+        case 0xA3:
+        case 0xA4:
+        case 0xA5:
+        case 0xA7:
+        case 0xA8:
+        case 0xA9:
+        case 0xAA:
+        case 0xAB:
+        case 0xAC:
+        case 0xAD:
+        case 0xAF:
+        case 0xB0:
+        case 0xB1:
+        case 0xB2:
+        case 0xB3:
+        case 0xB4:
+        case 0xB5:
+        case 0xB7:
+        case 0xB8:
+        case 0xB9:
+        case 0xBA:
+        case 0xBB:
+        case 0xBC:
+        case 0xBD:
+        case 0xBF:
+            register_set_or_unset_bit(*alt, 0x01 << (reg.byte_value >> 3 & 0x07), false);
+            *z80_decode_reg8(reg, 0, &hl) = *alt;
+            return 23; // TODO ?
         case 0x86: // RES b,(IX+d)
         case 0x8E:
         case 0x96:
@@ -2088,6 +2146,65 @@ int z80_execute_dd_fd(REG8 reg, REG16 *other)
         case 0xBE:
             register_set_or_unset_bit(*alt, 0x01 << (reg.byte_value >> 3 & 0x07), false);
             return 23;
+        case 0xC0: // LD r,SET b,(IX+d)
+        case 0xC1:
+        case 0xC2:
+        case 0xC3:
+        case 0xC4:
+        case 0xC5:
+        case 0xC7:
+        case 0xC8:
+        case 0xC9:
+        case 0xCA:
+        case 0xCB:
+        case 0xCC:
+        case 0xCD:
+        case 0xCF:
+        case 0xD0:
+        case 0xD1:
+        case 0xD2:
+        case 0xD3:
+        case 0xD4:
+        case 0xD5:
+        case 0xD7:
+        case 0xD8:
+        case 0xD9:
+        case 0xDA:
+        case 0xDB:
+        case 0xDC:
+        case 0xDD:
+        case 0xDF:
+        case 0xE0:
+        case 0xE1:
+        case 0xE2:
+        case 0xE3:
+        case 0xE4:
+        case 0xE5:
+        case 0xE7:
+        case 0xE8:
+        case 0xE9:
+        case 0xEA:
+        case 0xEB:
+        case 0xEC:
+        case 0xED:
+        case 0xEF:
+        case 0xF0:
+        case 0xF1:
+        case 0xF2:
+        case 0xF3:
+        case 0xF4:
+        case 0xF5:
+        case 0xF7:
+        case 0xF8:
+        case 0xF9:
+        case 0xFA:
+        case 0xFB:
+        case 0xFC:
+        case 0xFD:
+        case 0xFF:
+            register_set_or_unset_bit(*alt, 0x01 << (reg.byte_value >> 3 & 0x07), true);
+            *z80_decode_reg8(reg, 0, &hl) = *alt;
+            return 23; // TODO ?
         case 0xC6: // SET b,(IX+d)
         case 0xCE:
         case 0xD6:
